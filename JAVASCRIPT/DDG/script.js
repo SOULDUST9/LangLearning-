94ef93f58434c163735ebdec58d6dcf2129ec779
Sebastian Janeczek <sjane089@uottawa.ca>
2025-07-09 14:39:25 -0400

    added DDG javascript folder

---
+++ JAVASCRIPT/DDG/script.js
@@ -0,0 +1,9 @@
+//little duck duck goose game.
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber != 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🦢");
