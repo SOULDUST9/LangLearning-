ec8e7f1d131c0fe2a192be88a3d18ce38955dd6c
Sebastian Janeczek <sjane089@uottawa.ca>
2025-07-08 22:08:33 -0400

    added temperature javascript folder

---
+++ JAVASCRIPT/temperature/script.js
@@ -0,0 +1,6 @@
+let temp = 50; //set the tempature to 25 Fahrenheit.
+console.log("The temperature in Fahrenheit is: " + temp);
+
+//converting to celcius
+temp = (temp - 32) / 1.8;
+console.log("The temperature in Celsius is: " + temp);
