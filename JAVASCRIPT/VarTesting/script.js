8b378466c9684da2d33f8f51bef2d6cef3b41fad
Sebastian Janeczek <sjane089@uottawa.ca>
2025-07-08 21:55:02 -0400

    added VarTesting javascript folder

---
+++ JAVASCRIPT/VarTesting/script.js
@@ -0,0 +1,16 @@
+const name = "Sebastian";
+const favColor = "red";
+let location = "Quebec";
+let mood = "Angry";
+
+console.log(name);
+console.log(favColor);
+console.log(location);
+console.log(mood);
+
+location = "Ontario";
+mood = "Happy";
+
+console.log("");
+console.log(location);
+console.log(mood);
