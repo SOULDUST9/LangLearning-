94f5d79a80c00f3d7b3f317a61ae208220492239
Sebastian Janeczek <sjane089@uottawa.ca>
2025-07-14 07:54:42 -0400

    added basicOperation javascript folder

---
+++ JAVASCRIPT/basicOperations/script.js
@@ -0,0 +1,16 @@
+function multiply(a, b){
+    return a*b;
+  }
+  function add(a, b){
+    return a+b;
+  }
+  function subtract(a, b){
+    return a-b;
+  }
+  function divid(a, b){
+    return a/b;
+  }
+  console.log(multiply(2,4));
+  console.log(add(3,1));
+  console.log(subtract(0,5));
+  console.log(divid(10,4));
