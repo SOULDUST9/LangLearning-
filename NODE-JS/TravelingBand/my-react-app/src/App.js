054a6d1b8b1015a8fce39f86d6a82b41077300dc
Sebastian Janeczek <sjane089@uottawa.ca>
2025-08-04 12:47:39 -0400

    added travelingBand to node-js folder

---
+++ NODE-JS/TravelingBand/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Places from "./Places"
+
+function App() {
+  return (
+    <>
+      <h1>Come Meet our Band at these locations</h1>
+      <Places/>
+    </>
+  );
+}
+
+export default App;
