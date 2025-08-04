d3ae19cbf9991ba11ecca4ebcbe81ef349e81b61
Sebastian Janeczek <sjane089@uottawa.ca>
2025-08-04 12:18:07 -0400

    added componentsTest to node-js folder

---
+++ NODE-JS/componentsTest/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greetings from "./greetings.js";
+
+function App() {
+  return (
+    <>
+      <h1>HELLO</h1>
+      <Greetings/>
+      <Greetings/>
+    </>
+  );
+}
+
+export default App;
---
+++ NODE-JS/componentsTest/my-react-app/src/greetings.js
@@ -0,0 +1,12 @@
+import "./greetings.css"
+
+function Greetings() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>Nice to meet you. 😊</p>
+      </div>
+    )
+}
+
+export default Greetings;
