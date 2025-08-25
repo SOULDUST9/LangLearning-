04b7297b2457dfb272d0bba6ff0cdbd2286e0b42
Sebastian Janeczek <sjane089@uottawa.ca>
2025-08-25 11:23:16 -0400

    commententized (React.js): Menu/ItemsContainer.js

--- REACT.JS/Menu/my-react-app/src/components/ItemsContainer.js
+++ REACT.JS/Menu/my-react-app/src/components/ItemsContainer.js
@@ -1,18 +1,18 @@
-import Item from "./Item.js";
+// import Item from "./Item.js";
 
-function ItemContainer({ listA, listB, listC }) {
-    return (
-        <>
-        {listA.map((image, index) => (
-            <Item
-            key={index} 
-            image={image}
-            title={listB[index]}
-            desc={listC[index]}
-            />
-        ))}
-        </>
-    );
-}
+// function ItemContainer({ listA, listB, listC }) {
+//     return (
+//         <>
+//         {listA.map((image, index) => (
+//             <Item
+//             key={index} 
+//             image={image}
+//             title={listB[index]}
+//             desc={listC[index]}
+//             />
+//         ))}
+//         </>
+//     );
+// }
 
-export default ItemContainer;
+// export default ItemContainer;
